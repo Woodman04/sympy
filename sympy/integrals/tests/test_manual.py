e14fe361a9dfe8753f92f89566a02361af066c4c
Edoardo Matachione <edoardomatachione@gmail.com>
2026-02-28 00:31:58 +0100

    integrals: avoid use of equals() in tests

--- sympy/integrals/tests/test_manual.py
+++ sympy/integrals/tests/test_manual.py
@@ -752,18 +752,16 @@ def test_manualintegrate_sqrt_fractional_linear():
     assert F2.diff(x).equals(f.subs(a, 0))
     # linear dependent bases (2*x+2)/(x-1) and (x+1)/(x-1)
     f = ((2*x+2)/(x-1))**(S.One/4)*sqrt(((x+1)/(x-1)))
-    F = (-8*2**(S.One/4)*(-((x + 1)/(x - 1))**(S.One/4)/(8*(sqrt((x + 1)/(x - 1)) + 1))
+    F = (-8*2**(S.One/4)*(-((x + 1)/(x - 1))**(S.One/4)*S(1)/8/(sqrt((x + 1)/(x - 1)) + 1)
          + 3*log(((x + 1)/(x - 1))**(S.One/4) - 1)/16 - 3*log(((x + 1)/(x - 1))**(S.One/4) + 1)/16
-         + 3*atan(((x + 1)/(x - 1))**(S.One/4))/8 - 1/(16*(((x + 1)/(x - 1))**(S.One/4) + 1))
-         - 1/(16*(((x + 1)/(x - 1))**(S.One/4) - 1))))
-    assert manualintegrate(f, x).equals(F)
-    assert F.diff(x).equals(f)
+         + 3*atan(((x + 1)/(x - 1))**(S.One/4))/8 - S(1)/16/(((x + 1)/(x - 1))**(S.One/4) + 1)
+         - S(1)/16/(((x + 1)/(x - 1))**(S.One/4) - 1)))
+    assert_is_integral_of(f, F)
     # constant values sqrt((2*x + 4)/(6*x + 12)) and ((2*a*x + 6*b) / (a*x + 3*b))**(S.One/3)
     f = sqrt((2*x + 4)/(6*x + 12))*((2*a*x + 6*b) / (a*x + 3*b))**(S.One/3)*sqrt(((x+1)/(x-1)))
     F = (-4*2**(S.One/3)*sqrt(3)*(log(sqrt((x + 1)/(x - 1)) - 1)/4 - log(sqrt((x + 1)/(x - 1)) + 1)/4
-        - 1/(4*(sqrt((x + 1)/(x - 1)) + 1)) - 1/(4*(sqrt((x + 1)/(x - 1)) - 1)))/3)
-    assert manualintegrate(f, x).equals(F)
-    assert F.diff(x).equals(f)
+        - S(1)/4/(sqrt((x + 1)/(x - 1)) + 1) - S(1)/4/(sqrt((x + 1)/(x - 1)) - 1)))/3
+    assert_is_integral_of(f, F)
 
 
 def test_manualintegrate_sqrt_quadratic():
@@ -783,13 +781,13 @@ def test_manualintegrate_sqrt_quadratic():
                           -7*sqrt(-3*x**2 + 4*x + 5)/3 + 32*sqrt(3)*asin(3*sqrt(19)*(x - S(2)/3)/19)/9)
     assert_is_integral_of((7*x+6)/sqrt(3*x**2+4*x-5),
                           7*sqrt(3*x**2 + 4*x - 5)/3 + 4*sqrt(3)*log(6*x + 2*sqrt(3)*sqrt(3*x**2 + 4*x - 5) + 4)/9)
-    assert manualintegrate((d+e*x)/sqrt(a+b*x+c*x**2), x).equals(
+    assert manualintegrate((d+e*x)/sqrt(a+b*x+c*x**2), x) == \
         Piecewise(((-b*e/(2*c) + d) *
                    Piecewise((log(b + 2*sqrt(c)*sqrt(a + b*x + c*x**2) + 2*c*x)/sqrt(c), Ne(a - b**2/(4*c), 0)),
                              ((b/(2*c) + x)*log(b/(2*c) + x)/sqrt(c*(b/(2*c) + x)**2), True)) +
                    e*sqrt(a + b*x + c*x**2)/c, Ne(c, 0)),
-                  ((2*d*sqrt(a + b*x) + 2*e*(-a*sqrt(a + b*x) + (a + b*x)**(S(3)/2)/3)/b)/b, Ne(b, 0)),
-                  ((d*x + e*x**2/2)/sqrt(a), True)))
+                  (2*((d*sqrt(a + b*x) - e*(a*sqrt(a + b*x) - (a + b*x)**(S(3)/2)/3)/b)/b), Ne(b, 0)),
+                  ((d*x + e*x**2/2)/sqrt(a), True))
 
     assert manualintegrate((3*x**3-x**2+2*x-4)/sqrt(x**2-3*x+2), x) == \
         sqrt(x**2 - 3*x + 2)*(x**2 + 13*x/4 + S(101)/8) + 135*log(2*x + 2*sqrt(x**2 - 3*x + 2) - 3)/16
